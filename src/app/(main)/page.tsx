929eda8fdc075dc27474f7398341319214329518
ziadx3 <hatemziad384@gmail.com>
2024-05-26 12:52:52 +0300

    TEST

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,12 @@ import { Content } from "@/components/main/content";
 import { LanguagesBar } from "@/components/main/languages-bar/languages-bar";
 
 export default function Home() {
-  return (
-    <main className="h-screen w-full">
+  return <h1>cool</h1>;
+}
+
+/*
+
+<main className="h-screen w-full">
       <div className="container mx-auto h-[calc(100vh-5rem)]">
         <Header />
         <div className="h-[calc(100%-5rem)] w-full">
@@ -13,5 +17,5 @@ export default function Home() {
       </div>
       <LanguagesBar />
     </main>
-  );
-}
+
+*/
