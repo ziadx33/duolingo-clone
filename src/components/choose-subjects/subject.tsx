5e8d0ba1e378cbc2f3d797077ac027bd5b13eac7
ziadx3 <hatemziad384@gmail.com>
2024-05-26 16:13:06 +0300

    fixed

--- src/components/choose-subjects/subject.tsx
+++ src/components/choose-subjects/subject.tsx
@@ -5,23 +5,21 @@ import { getServerAuthSession } from "@/server/auth";
 
 export async function Subject({ code, name, id }: Subject) {
   const userData = await getServerAuthSession();
-  console.log(userData?.user);
-  // const isAlreadyChosen = (user?.currentSubjectId ?? "") === id;
+  const isAlreadyChosen = (userData?.user?.currentSubjectId ?? "") === id;
   return (
-    // <SubjectButton
-    //   userData={user}
-    //   isAlreadyChosen={!!isAlreadyChosen}
-    //   subjectId={id}
-    // >
-    //   <Image
-    //     src={`/images/flags/${code}.svg`}
-    //     alt={code}
-    //     width={100}
-    //     height={100}
-    //     draggable="false"
-    //   />
-    //   <h3 className="text-center">{name}</h3>
-    // </SubjectButton>
-    <h1>hi</h1>
+    <SubjectButton
+      userData={userData?.user}
+      isAlreadyChosen={!!isAlreadyChosen}
+      subjectId={id}
+    >
+      <Image
+        src={`/images/flags/${code}.svg`}
+        alt={code}
+        width={100}
+        height={100}
+        draggable="false"
+      />
+      <h3 className="text-center">{name}</h3>
+    </SubjectButton>
   );
 }
