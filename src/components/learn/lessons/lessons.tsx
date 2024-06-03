9491d6be0ecf1add8e90eb2bf2dba283bada6794
ziadx3 <hatemziad384@gmail.com>
2024-06-03 17:45:24 +0300

    fix: fixed production errors

--- src/components/learn/lessons/lessons.tsx
+++ src/components/learn/lessons/lessons.tsx
@@ -3,7 +3,8 @@ import { api } from "@/trpc/server";
 import { Unit } from "./units/unit";
 
 export async function Lessons() {
-  const { user } = await getServerAuthSession();
+  const userData = await getServerAuthSession();
+  const user = userData?.user;
   if (!user) return null;
   const units = await api.units.getUnitsBySubjectId({
     subjectId: user.currentSubjectId,
